103ee6132192b7fb457f8bb3bfbc0ba6ce3ea102
Yoo-Ha-Young <bluekitten74@naver.com>
2023-06-05 18:03:59 +0900

    [doc update]

---
+++ popit/src/main/java/com/popit/popitproject/common/IndexController.java
@@ -0,0 +1,16 @@
+package com.popit.popitproject.common;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping    // 기본 홈 경로
+    public ResponseBody<?> index() {
+        return ResponseEntity.ok();
+    }
+
+}
