89a41a5341b8de9c08827ae486b6d71dfb1457b6
Yoo-Ha-Young <bluekitten74@naver.com>
2023-06-05 17:50:08 +0900

    [doc update]

---
+++ popit/src/main/java/com/popit/popitproject/PopitApplication.java
@@ -0,0 +1,13 @@
+package com.popit.popitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopitApplication.class, args);
+	}
+
+}
---
+++ popit/src/test/java/com/popit/popitproject/PopitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popit.popitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
