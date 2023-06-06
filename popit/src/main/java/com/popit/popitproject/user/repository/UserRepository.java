8298e2a809f9efe62c2cde05198217fe2f4afe09
yongsuKim <yongsu0312.kim@gmail.com>
2023-06-06 20:53:45 +0900

    user/mockup ---
    1. mockup Test를 위해 커밋 및 푸시
    2. 회원가입, 로그인 기능 mockup 형태로 구현

--- popit/src/main/java/com/popit/popitproject/user/repository/UserRepository.java
+++ popit/src/main/java/com/popit/popitproject/user/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.popit.popitproject.user.repository;
 
-public class UserRepository {
+import com.popit.popitproject.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    UserEntity findByUserId(String userId);
 }
