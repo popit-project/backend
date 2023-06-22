528184eade3bbc2fb95b944d598548f5b06b3030
DESKTOP-PG27TQQ\jjang <jjang990312@naver.com>
2023-06-23 01:43:04 +0900

    리뷰-추가와 출력

--- popit/src/main/java/com/popit/popitproject/store/entity/StoreEntity.java
+++ popit/src/main/java/com/popit/popitproject/store/entity/StoreEntity.java
@@ -1,5 +1,93 @@
 package com.popit.popitproject.store.entity;
 
-public class StoreEntity {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.popit.popitproject.review.entity.ReviewEntity;
+import com.popit.popitproject.store.model.StoreType;
+import com.popit.popitproject.store.repository.MapMapping;
+import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity(name = "store")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreEntity implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    // 입점신청 : 고정값
+    @Column(unique = true)
+    private String sellerId; // 판매자 ID
+
+    @Column(name = "store_name")
+    private String storeName;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private StoreType storeType; // 사업 종류
+
+    @Column
+    private String businessLicenseAddress; // 사업자 등록증 내 주소
+
+    @Column
+    private String businessLicenseNumber; // 사업자 등록번호
+
+    @Column
+    private Timestamp enteredAt; // 입점 날짜
+
+    @Column
+    private Timestamp updatedAt;
+
+    @Column
+    private String storePhone;
+
+    // 새로운 팝업에 대한 수정/등록
+    private String image; // 매장 이미지
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime openTime; // 운영시간
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime closeTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate openDate; // 운영기간
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate closeDate;
+
+
+    @Column(name = "store_address")
+    private String storeAddress; // 매장 주소
+
+    // map 등록을 위한 위경도
+    @Column(name = "x")
+    private Double x;
+
+    @Column(name = "y")
+    private Double y;
+
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ReviewEntity> comments;
+
+    public static StoreEntity from (MapMapping mapMapping) {
+        return StoreEntity.builder()
+                .id(mapMapping.getId())
+                .storeName(mapMapping.getStoreName())
+                .build();
+    }
 }
+
