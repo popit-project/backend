1795fc305a7db81694d7ce6f03387f0dd9e8fb06
“aidenk0312” <yongsu0312.kim@gmail.comgit config --global user.email yongsu0312.kim@gmail.com>
2023-06-23 02:54:57 +0900

    Swagger, 기능별 접속 권한 설정

--- popit/src/main/java/com/popit/popitproject/config/SecurityConfig.java
+++ popit/src/main/java/com/popit/popitproject/config/SecurityConfig.java
@@ -18,20 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable() // 추후 셀러 페이지 합칠 경우 코드 수정 필요
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/user/register").permitAll()
-                .antMatchers("/api/user/validate-email").permitAll()
-                .antMatchers("/api/user/login").permitAll()
-                .antMatchers("/api/user/logout").permitAll()
-                .antMatchers("/api/user/info").permitAll()
-                .antMatchers("/api/user/find-id").permitAll()
-                .antMatchers("/api/user/reset-password").permitAll()
-                .antMatchers("/api/user/change-password").permitAll()
-                .antMatchers("/api/login/google").permitAll()
-                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll() // swagger
-                .antMatchers("/user/**").hasAnyAuthority("ROLE_USER")
-                .antMatchers("/seller/**").hasAnyAuthority("ROLE_USER", "ROLE_SELLER")
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                        "/configuration/security", "/swagger-ui.html", "/swagger-ui/**", "/webjars/**").permitAll()
+                .antMatchers("/user/**").hasAnyAuthority("ROLE_USER", "ROLE_SELLER")
+                .antMatchers("/seller/**").hasAnyAuthority("ROLE_SELLER")
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
