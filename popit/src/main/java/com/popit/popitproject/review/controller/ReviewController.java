7850ae16d93365117c91054ef24a43ee1022ba6c
DESKTOP-PG27TQQ\jjang <jjang990312@naver.com>
2023-06-26 17:37:11 +0900

    review - 수정,삭제 추가 + swagger 설명

--- popit/src/main/java/com/popit/popitproject/review/controller/ReviewController.java
+++ popit/src/main/java/com/popit/popitproject/review/controller/ReviewController.java
@@ -23,7 +23,6 @@ public class ReviewController {
     @PostMapping("/write/{storeId}")
     public ResponseEntity<String> createComment(@PathVariable("storeId") Long storeId, @RequestBody CreateReviewRequest createReviewRequest) {
         reviewService.createComment(storeId,createReviewRequest);
-
         return ResponseEntity.ok("review write success!");
     }
 
