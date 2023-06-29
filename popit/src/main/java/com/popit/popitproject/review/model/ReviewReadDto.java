27aca9b1e6bd5ccc3ec4c52a2725bd63a5aaf46a
DESKTOP-PG27TQQ\jjang <jjang990312@naver.com>
2023-06-29 18:46:51 +0900

    review - read reviewID 추출

--- popit/src/main/java/com/popit/popitproject/review/model/ReviewReadDto.java
+++ popit/src/main/java/com/popit/popitproject/review/model/ReviewReadDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReviewReadDto {
+    private Long id;
     private String comment;
     private String email;
 }
