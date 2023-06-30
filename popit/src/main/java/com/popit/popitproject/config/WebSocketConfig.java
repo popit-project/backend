ac9f1e907ed8ac04cd362763d040ccece5b2e9e1
“aidenk0312” <yongsu0312.kim@gmail.comgit config --global user.email yongsu0312.kim@gmail.com>
2023-06-30 22:49:39 +0900

    news/websoket-modefy2 ---
    - 오류 해결을 위한 연결 부분 ws 로 재변경

--- popit/src/main/java/com/popit/popitproject/config/WebSocketConfig.java
+++ popit/src/main/java/com/popit/popitproject/config/WebSocketConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Overrided
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
