942ea6b7a2a1e750d3cb8f0ba87407cea06c310b
“aidenk0312” <yongsu0312.kim@gmail.comgit config --global user.email yongsu0312.kim@gmail.com>
2023-07-01 18:11:56 +0900

    seller/enter-modify - sellerId Info 되게 추가

--- popit/src/main/java/com/popit/popitproject/store/controller/StoreSellerController.java
+++ popit/src/main/java/com/popit/popitproject/store/controller/StoreSellerController.java
@@ -51,70 +51,102 @@ public class StoreSellerController {
 
 
     @ApiOperation(
-        value = "판매자 입점신청"
-        , notes = "로그인 후, 해당 사용자의 정보로 가게 입점신청을 진행하고 가게를 생성합니다.")
+            value = "판매자 입점신청"
+            , notes = "로그인 후, 해당 사용자의 정보로 가게 입점신청을 진행하고 가게를 생성합니다.")
     @PostMapping(path = "/sellerEnter", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> sellerEntered(@AuthenticationPrincipal String userId,
-        @RequestPart("file") MultipartFile file,
-        @RequestPart("sellerDTO") StoreSellerDTO sellerDTO) throws IOException {
+                                           @RequestPart(name = "file") MultipartFile file,
+                                           @RequestPart(name = "sellerDTO") StoreSellerDTO sellerDTO) throws IOException {
 
         // 유효성 검사
         UserEntity user = storeSellerValidate.validateSellerRegistration(userId, sellerDTO);
 
         // 서비스를 이용해서 Seller 생성
         StoreEntity createdSeller = sellerService.saveSellerInfo(file, user,
-            StoreSellerDTO.toEntity(sellerDTO));
+                StoreSellerDTO.toEntity(sellerDTO));
         sellerService.generateSellerRole(user, createdSeller);
 
         String newAddress = createdSeller.getStoreAddress();
         StoreEntity change = KakaoAddressChange.addressChange(newAddress);
 
         SellerResponse sellerResponse = SellerResponse.builder()
-            .id(createdSeller.getId())
-            .storeName(createdSeller.getStoreName())
-            .storeImage(createdSeller.getStoreImage())
-            .storeType(String.valueOf(createdSeller.getStoreType()))
-            .storeAddress(createdSeller.getStoreAddress())
-            .openTime(createdSeller.getOpenTime())
-            .closeTime(createdSeller.getCloseTime())
-            .openDate(createdSeller.getOpenDate())
-            .closeDate(createdSeller.getCloseDate())
-            .businessLicenseNumber(createdSeller.getBusinessLicenseNumber())
-            .x(change.getX())
-            .y(change.getY())
-            .build();
+                .id(createdSeller.getId())
+                .storeName(createdSeller.getStoreName())
+                .storeImage(createdSeller.getStoreImage())
+                .storeType(String.valueOf(createdSeller.getStoreType()))
+                .storeAddress(createdSeller.getStoreAddress())
+                .openTime(createdSeller.getOpenTime())
+                .closeTime(createdSeller.getCloseTime())
+                .openDate(createdSeller.getOpenDate())
+                .closeDate(createdSeller.getCloseDate())
+                .businessLicenseNumber(createdSeller.getBusinessLicenseNumber())
+                .x(change.getX())
+                .y(change.getY())
+                .build();
         return ResponseEntity.ok().body(sellerResponse);
     }
 
+
     @ApiOperation(
-        value = "스토어 프로필 홈"
-        , notes = "입점 신청 후 생성된 가게의 정보를 줍니다.")
-    @GetMapping("/store/{storeName}/storeHome")
-    public ResponseEntity<?> sellerStoreInfo(@PathVariable("storeName") String storeName) {
-        StoreEntity store = storeRepository.findByStoreName(storeName).orElseThrow(
-            () -> new RuntimeException("매장을 찾을 수 없습니다.")
+            value = "스토어 유저용 프로필 홈"
+            , notes = "입점 신청 후 생성된 가게의 정보를 줍니다.")
+    @GetMapping("/store/{storeId}/storeHome")
+    public ResponseEntity<?> userStoreInfo(@PathVariable("storeId") Long storeId) {
+
+        StoreEntity store = storeRepository.findById(storeId).orElseThrow(
+                () -> new RuntimeException("매장을 찾을 수 없습니다.")
         );
 
         SellerStoreHomeResponse sellerResponse = SellerStoreHomeResponse.builder()
-            .storeId(store.getId())
-            .sellerId(store.getUser().getStore().getId())
-            .storeImage(store.getStoreImage())
-            .storeName(store.getStoreName())
-            .storeType(store.getStoreType().getDisplayName())
-            .storeAddress(store.getStoreAddress())
-            .openTime(store.getOpenTime())
-            .closeTime(store.getCloseTime())
-            .openDate(store.getOpenDate())
-            .closeDate(store.getCloseDate())
-            .build();
+                .storeId(store.getId())
+                .sellerId(store.getUser().getStore().getId())
+                .storeImage(store.getStoreImage())
+                .storeName(store.getStoreName())
+                .storeType(store.getStoreType().getDisplayName())
+                .storeAddress(store.getStoreAddress())
+                .openTime(store.getOpenTime())
+                .closeTime(store.getCloseTime())
+                .openDate(store.getOpenDate())
+                .closeDate(store.getCloseDate())
+                .build();
 
         return ResponseEntity.ok().body(sellerResponse);
 
     }
 
     @ApiOperation(
-        value = "가게 삭제"
-        , notes = "가게를 삭제하고, 그 안의 리뷰, 소식도 함께 삭제합니다.")
+            value = "스토어 셀러용 프로필 홈"
+            , notes = "입점 신청 후 생성된 가게의 정보를 줍니다.")
+    @GetMapping("가")
+    public ResponseEntity<?> sellerStoreInfo(@AuthenticationPrincipal String userId,
+                                             @PathVariable("sellerId") Long sellerId) {
+
+        UserEntity user = userRepository.findByUserId(userId);
+        StoreEntity store = storeRepository.findById(user.getStore().getId()).orElseThrow(
+                () -> new RuntimeException("매장을 찾을 수 없습니다.")
+        );
+
+        SellerStoreHomeResponse sellerResponse = SellerStoreHomeResponse.builder()
+                .sellerId(sellerId)
+                .storeId(store.getId())
+                .sellerId(store.getUser().getStore().getId())
+                .storeImage(store.getStoreImage())
+                .storeName(store.getStoreName())
+                .storeType(store.getStoreType().getDisplayName())
+                .storeAddress(store.getStoreAddress())
+                .openTime(store.getOpenTime())
+                .closeTime(store.getCloseTime())
+                .openDate(store.getOpenDate())
+                .closeDate(store.getCloseDate())
+                .build();
+
+        return ResponseEntity.ok().body(sellerResponse);
+
+    }
+
+    @ApiOperation(
+            value = "가게 삭제"
+            , notes = "가게를 삭제하고, 그 안의 리뷰, 소식도 함께 삭제합니다.")
     @DeleteMapping("/stores/{storeId}")
     public ResponseEntity<String> deleteStore(@PathVariable Long storeId) {
         try {
@@ -124,22 +156,22 @@ public class StoreSellerController {
             return ResponseEntity.ok("Store deleted successfully");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Failed to delete store: " + e.getMessage());
+                    .body("Failed to delete store: " + e.getMessage());
         }
     }
 
     @ApiOperation(
-        value = "판매자 입점 정보 수정"
-        , notes = "가게 입점 정보를 수정합니다.")
+            value = "판매자 입점 정보 수정"
+            , notes = "가게 입점 정보를 수정합니다.")
     @PutMapping("/seller/sellerEnter")
     public ResponseEntity<?> updateSellerInfo(@AuthenticationPrincipal String userId,
-        @RequestBody UpdateStoreSellerDTO updatedStore) throws IOException {
+                                              @RequestBody UpdateStoreSellerDTO updatedStore) throws IOException {
 
         // 토큰에서 가져온 사용자 정보
         UserEntity user = userRepository.findByUserId(userId);
 
         StoreEntity storeInfo = storeRepository.findByUser(user)
-            .orElseThrow(() -> new IllegalArgumentException("Seller not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Seller not found"));
 
         String newAddress = updatedStore.getStoreAddress();
         StoreEntity change = KakaoAddressChange.addressChange(newAddress);
@@ -157,17 +189,17 @@ public class StoreSellerController {
         StoreEntity updatedStoreInfo = storeRepository.save(storeInfo);
 
         SellerResponse sellerResponse = SellerResponse.builder()
-            .id(updatedStoreInfo.getId())
-            .storeName(updatedStoreInfo.getStoreName())
-            .storeImage(updatedStoreInfo.getStoreImage())
-            .storeType(String.valueOf(updatedStoreInfo.getStoreType()))
-            .storeAddress(updatedStoreInfo.getStoreAddress())
-            .openTime(updatedStoreInfo.getOpenTime())
-            .closeTime(updatedStoreInfo.getCloseTime())
-            .openDate(updatedStoreInfo.getOpenDate())
-            .closeDate(updatedStoreInfo.getCloseDate())
-            .businessLicenseNumber(updatedStoreInfo.getBusinessLicenseNumber())
-            .build();
+                .id(updatedStoreInfo.getId())
+                .storeName(updatedStoreInfo.getStoreName())
+                .storeImage(updatedStoreInfo.getStoreImage())
+                .storeType(String.valueOf(updatedStoreInfo.getStoreType()))
+                .storeAddress(updatedStoreInfo.getStoreAddress())
+                .openTime(updatedStoreInfo.getOpenTime())
+                .closeTime(updatedStoreInfo.getCloseTime())
+                .openDate(updatedStoreInfo.getOpenDate())
+                .closeDate(updatedStoreInfo.getCloseDate())
+                .businessLicenseNumber(updatedStoreInfo.getBusinessLicenseNumber())
+                .build();
 
         return ResponseEntity.ok().body(sellerResponse);
     }
