9470ac53dc23b6733bac3f948adf3133b5889968
DESKTOP-PG27TQQ\jjang <jjang990312@naver.com>
2023-07-06 04:41:31 +0900

    common - index로 다시 변경

--- popit/src/main/java/com/popit/popitproject/common/IndexController.java
+++ popit/src/main/java/com/popit/popitproject/common/IndexController.java
@@ -1,19 +1,16 @@
 package com.popit.popitproject.common;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class IndexController {
 
-    @GetMapping("/health-check")    // 기본 홈 경로
-    public ResponseEntity<Void> checkHealthStatus() {
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping    // 기본 홈 경로
+    public String index() {
+        return "index";
     }
 
 }
