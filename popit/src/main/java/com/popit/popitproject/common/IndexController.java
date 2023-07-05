ea405b2153013e3963dde7efc6334ef05e2a0074
DESKTOP-PG27TQQ\jjang <jjang990312@naver.com>
2023-07-06 04:12:55 +0900

    common - 경로 변경

--- popit/src/main/java/com/popit/popitproject/common/IndexController.java
+++ popit/src/main/java/com/popit/popitproject/common/IndexController.java
@@ -1,16 +1,19 @@
 package com.popit.popitproject.common;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class IndexController {
 
-    @GetMapping    // 기본 홈 경로
-    public String index() {
-        return "index";
+    @GetMapping("/health-check")    // 기본 홈 경로
+    public ResponseEntity<Void> checkHealthStatus() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
