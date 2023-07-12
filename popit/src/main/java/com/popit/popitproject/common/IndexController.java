974870a486529dad113ccb8f2924856630bd355c
DESKTOP-PG27TQQ\jjang <jjang990312@naver.com>
2023-07-12 20:15:18 +0900

    common - Jenkins Test27

--- popit/src/main/java/com/popit/popitproject/common/IndexController.java
+++ popit/src/main/java/com/popit/popitproject/common/IndexController.java
@@ -1,8 +1,6 @@
 package com.popit.popitproject.common;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +8,7 @@ public class IndexController {
 
     @GetMapping    // 기본 홈 경로
     public String index() {
-        return "jenkins Test26";
+        return "jenkins Test27";
     }
 
 }
